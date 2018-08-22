e53a414e644fd304f2a00514b7c3f2e53ce3084d
charjay <820419196@qq.com>
2018-08-22 18:35:44 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 
-#### spring-simpleioc IOC的简单实现，包含控制反转与依赖注入
-#### spring-simpleaop AOP的简单实现
+### 项目结构
+1. spring-simpleioc IOC的简单实现，包含控制反转与依赖注入
+2. spring-simpleaop AOP的简单实现
+3. spring-simplemvc MVC的简单实现
+4. spring-jdbc-ext  JDBC的二次封装
--- spring-jdbc-ext/src/main/java/com/spring/jdbc/framework/BaseDaoSupport.java
+++ spring-jdbc-ext/src/main/java/com/spring/jdbc/framework/BaseDaoSupport.java
@@ -38,7 +38,6 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
